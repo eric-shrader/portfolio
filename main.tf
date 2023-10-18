53ee151fa11cb79bf31c5bd3ad7074475edb2e02
eric-shrader <ericshrader101@gmail.com>
2023-10-18 17:24:12 -0500

    added cloudfront to terraform configuration

--- main.tf
+++ main.tf
@@ -18,17 +18,17 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "portfolio_bucket" {
+resource "aws_s3_bucket" "website_bucket" {
   bucket = "eric-shrader.com"
 
   tags = {
     Name        = "eric-shrader.com"
-    Environment = "Production"
+    Environment = "production"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.portfolio_bucket.id
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -39,12 +39,12 @@ resource "aws_s3_bucket_website_configuration" "example" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.portfolio_bucket.id
+resource "aws_s3_bucket_public_access_block" "allow_public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
 }
 
 resource "aws_s3_bucket_policy" "allow_public_access" {
-  bucket = aws_s3_bucket.portfolio_bucket.id
+  bucket = aws_s3_bucket.website_bucket.id
   policy = data.aws_iam_policy_document.allow_public_access.json
 }
 
@@ -61,4 +61,72 @@ data "aws_iam_policy_document" "allow_public_access" {
 
     resources = ["arn:aws:s3:::eric-shrader.com/*"]
   }
+}
+
+resource "aws_s3_object" "files" {
+  for_each = fileset("./build/", "**")
+  bucket   = aws_s3_bucket.website_bucket.bucket
+  key      = each.value
+  source   = "./build/${each.value}"
+  etag     = filemd5("./build/${each.value}")
+
+  # Determine the Content-Type based on the file extension
+  content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "jpg"  = "image/jpeg",
+    "png"  = "image/png",
+    "pdf"  = "application/pdf"
+  }, element(split(".", each.value), length(split(".", each.value)) - 1), "application/octet-stream")
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+    origin_id   = aws_s3_bucket.website_bucket.bucket
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "distribution for eric-shrader.com"
+  default_root_object = "index.html"
+  # aliases             = ["eric-shrader.com", "www.eric-shrader.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.website_bucket.bucket
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
 }
